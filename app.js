a3ce38a5f61e96c7640a629b4faa2b37f9696cc9
nik frank <nikbfrank@gmail.com>
2015-08-07 00:43:20 +0300

    client side routing

--- app.js
+++ app.js
@@ -1,35 +1,68 @@
 'use strict';
 
 angular.module('cpl', ['ngMaterial', 'ui.router'])
+    .config(function ($urlRouterProvider){
+	$urlRouterProvider.deferIntercept();
+    })
+    .run(function ($rootScope, $urlRouter, $location, $state) {
+	window.initPath = $location.path();
+	window.loadPath = '/';
+	$rootScope.$on('$locationChangeSuccess', function(e, newUrl){
+	    e.preventDefault();
+
+	    var nuHash = newUrl.split('#')[1];
+	    if(nuHash === '/'){
+		setTimeout(function(){
+		    $location.path(window.loadPath);
+		    window.loadPath = window.initPath;
+		}, 30);
+		$urlRouter.sync();
+	    }
+	    else{
+		console.log('nuhash', nuHash);
+		$rootScope.$broadcast('newUrl', nuHash);
+	    }
+	});
+
+	$urlRouter.listen();
+	$location.path('/');
+	setTimeout(function(){
+	    $rootScope.$broadcast('newUrl', window.initPath);
+	    $location.path(window.initPath);
+	}, 60);
+    })
     .config(function($stateProvider, $urlRouterProvider){
 	
 	$urlRouterProvider.otherwise("/");
 
 	$stateProvider
 	    .state('home', {
-		url: "/",
+		url:'/',
 		templateUrl: "views/home.html",
 		controller: 'HomeCtrl as home'
 	    })
 	    .state('home.home', {
 		views: {
 		    'one': {
+			url:'/one',
 			templateUrl:'views/one.html',
 			controller:'OneCtrl as one'
 		    },
 		    'two': {
+			url:'/two',
 			templateUrl:'views/two.html',
 			controller:'TwoCtrl as two'
 		    },
 		    'three': {
+			url:'/three',
 			templateUrl:'views/three.html',
 			controller:'ThreeCtrl as three'
 		    },
 		    'four': {
+			url:'/four',
 			templateUrl:'views/four.html',
 			controller:'FourCtrl as four'
 		    }
 		}
 	    });
-
   });
--- views/home.js
+++ views/home.js
@@ -1,9 +1,11 @@
 'use strict';
 
 angular.module('cpl')
-  .controller('HomeCtrl', function($state){
+  .controller('HomeCtrl', function($state, $location, $scope, $rootScope){
 
       $state.go('home.home');
+console.log($location.path());
+      var that = this;
 
       this.toolItems = [
 	  {
@@ -22,5 +24,20 @@ angular.module('cpl')
 	      title:'four',
 	      content:'blblahahblahblah'
 	  }
-      ]
+      ];
+
+      $scope.$watch('selectedIndex', function(n, o){
+	  if(typeof n === 'undefined') return;
+	  console.log(n, o, '/'+that.toolItems[n].title);
+	  $location.path('/'+that.toolItems[n].title);
+      });
+
+
+      $rootScope.$on('newUrl', function(e, data){
+	  console.log('to', data, $scope.selectedIndex);
+	  for(var i=that.toolItems.length; i-->0;)
+	      if('/'+that.toolItems[i].title === data)
+		  if($scope.selectedIndex !== i)
+		      $scope.selectedIndex = i;
+      });
   });
